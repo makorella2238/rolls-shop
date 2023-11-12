d7d21c5cbf833d836b5b39ab7c716c351abdd849
makorella2238 <122471811+makorella2238@users.noreply.github.com>
2023-11-12 22:02:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cartRouter from "./cart/cartRouter.js";
 import favoriteRouter from "./favorite/favoriteRouter.js";
 import allItemsRouter from "./allItems/allItemsRouter.js";
 import profileRouter from "./Profile/profileRouter.js";
-import orderRouter from "./Order/orderRouter";
+import orderRouter from "/Order/orderRouter";
 
 const app = express()
 const PORT =  process.env.PORT || 3200
