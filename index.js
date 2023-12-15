768c3e05bde3094bd742eced7d6163e085eefde3
makorella2238 <122471811+makorella2238@users.noreply.github.com>
2023-12-15 21:16:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const DB_URL = process.env.MONGODB_URI || 'mongodb+srv://makorella:makorella2238
 
 app.use(cors({
     origin: "https://roll-shop.netlify.app",
-    //origin: "http://localhost:3000",
     credentials: true
 }));
 
