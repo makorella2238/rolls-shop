98fe8b220a975f59296e2174a7cf32591e16747e
makorella2238 <122471811+makorella2238@users.noreply.github.com>
2023-12-15 21:16:05 +0300

    Delete models/Car.js

--- models/Car.js
+++
@@ -1,9 +0,0 @@
-import {Schema, model} from "mongoose";
-
-const Car = new Schema({
-    img: {type: String, required: true},
-    title: {type: String, unique: true, required: true},
-    price: {type: Number, required: true},
-})
-
-export default model('Car', Car)
